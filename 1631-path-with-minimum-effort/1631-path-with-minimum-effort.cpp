c0bb0e38175f05fd743e426cb079dbfc94b59f33
Ayush Verma <49532650+ayuvgr8@users.noreply.github.com>
2023-09-17 05:25:34 +0530

    Time: 109 ms (70.96%), Space: 20.3 MB (36.54%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
@@ -1,4 +1,4 @@
-// // #include <iostream>
+// #include <iostream>
 // #include <vector>
 // #include <queue>
 // #include <limits>
@@ -16,35 +16,35 @@ public:
         vector<vector<int>> effort(rows, vector<int>(cols, INT_MAX)); // Initialize the effort matrix.
         priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>> pq;
         
-        effort[0][0] = 0;
-        pq.push({0, {0, 0}});
+        effort[0][0] = 0; // Starting cell has an effort of 0.
+        pq.push({0, {0, 0}}); // Push the starting cell into the priority queue.
         
         while (!pq.empty()) {
-            auto [curEffort, curPos] = pq.top();
+            auto [curEffort, curPos] = pq.top(); // Get the cell with the minimum effort.
             pq.pop();
             
-            int x = curPos.first;
-            int y = curPos.second;
+            int x = curPos.first; // Current cell's row.
+            int y = curPos.second; // Current cell's column.
             
             if (x == rows - 1 && y == cols - 1) {
-                return curEffort;
+                return curEffort; // If we reach the bottom-right cell, return the effort.
             }
             
             if (curEffort > effort[x][y]) {
-                continue;
+                continue; // Skip if we have already found a better path to this cell.
             }
             
             for (auto& dir : directions) {
-                int nx = x + dir[0];
-                int ny = y + dir[1];
+                int nx = x + dir[0]; // Next cell's row.
+                int ny = y + dir[1]; // Next cell's column.
                 
                 if (nx >= 0 && nx < rows && ny >= 0 && ny < cols) {
-                    int diff = abs(heights[x][y] - heights[nx][ny]);
-                    int newEffort = max(curEffort, diff);
+                    int diff = abs(heights[x][y] - heights[nx][ny]); // Calculate height difference.
+                    int newEffort = max(curEffort, diff); // Determine new effort required.
                     
                     if (newEffort < effort[nx][ny]) {
-                        effort[nx][ny] = newEffort;
-                        pq.push({newEffort, {nx, ny}});
+                        effort[nx][ny] = newEffort; // Update the effort for the next cell.
+                        pq.push({newEffort, {nx, ny}}); // Push the next cell into the priority queue.
                     }
                 }
             }
