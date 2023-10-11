3f1d02bfcb22a562abf0c5a9583427eba61f04f8
Ayush Verma <49532650+ayuvgr8@users.noreply.github.com>
2023-10-11 15:06:05 +0530

    Time: 91 ms (72.08%), Space: 73.9 MB (9.19%) - LeetHub

--- 1911-minimum-elements-to-add-to-form-a-given-sum/1911-minimum-elements-to-add-to-form-a-given-sum.cpp
+++ 1911-minimum-elements-to-add-to-form-a-given-sum/1911-minimum-elements-to-add-to-form-a-given-sum.cpp
@@ -18,3 +18,19 @@ public:
         return result;
     }
 };
+
+// Explanation:
+
+// We define a function minElements that takes a vector nums, an integer limit, and an integer goal as input.
+
+// We use a long long variable sum to store the sum of the elements in the nums array. This is to ensure that we handle large sums that may exceed the range of an int.
+
+// We calculate the sum of all elements in the nums array by iterating through it and adding each element to the sum.
+
+// We calculate the absolute difference diff between the goal and the current sum to find out how far we are from the target goal. We cast goal to a long long to avoid overflow issues.
+
+// To find the minimum number of elements to be added to reach the goal, we use integer division. We add limit - 1 to the diff and then divide the result by limit. This ensures that we round up to the nearest integer, which is equivalent to the ceiling function. This calculation gives us the minimum number of elements needed to add while respecting the limit.
+
+// We return the result as the answer, which represents the minimum number of elements required to make the sum of the array equal to the goal.
+
+// This solution effectively calculates the minimum number of elements to add, taking into account the limits and the goal, even for large input values.
