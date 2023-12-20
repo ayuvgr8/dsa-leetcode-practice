537acc788a01ad3be060aaa578078bd7cda45445
Ayush Verma <49532650+ayuvgr8@users.noreply.github.com>
2023-12-20 13:18:37 +0530

    Time: 101 ms (53.96%), Space: 70 MB (51.09%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,20 +1,20 @@
-// class Solution {
-// public:
-//     bool containsDuplicate(vector<int>& nums) {
-//         unordered_set<int> seen;  // Create an unordered_set to store seen elements
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> seen;  // Create an unordered_set to store seen elements
         
-//         for (int num : nums) {
-//             // If the element is already in the set, return true (duplicate found)
-//             if (seen.find(num) != seen.end()) {
-//                 return true;
-//             }
+        for (int num : nums) {
+            // If the element is already in the set, return true (duplicate found)
+            if (seen.find(num) != seen.end()) {
+                return true;
+            }
             
-//             seen.insert(num);  // Add the element to the set
-//         }
+            seen.insert(num);  // Add the element to the set
+        }
         
-//         return false;  // No duplicates found
-//     }
-// };
+        return false;  // No duplicates found
+    }
+};
 
 
 
@@ -49,15 +49,15 @@
 
 
 
-// Shorted Approach
-class Solution {
-public:
-    bool containsDuplicate(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        bool flag = false;
-        for(int i =0;i<nums.size()-1;i++){
-            if(nums[i] == nums[i+1]) return true;
-        }
-        return flag;
-    }
-};
+// // Shorted Approach
+// class Solution {
+// public:
+//     bool containsDuplicate(vector<int>& nums) {
+//         sort(nums.begin(),nums.end());
+//         bool flag = false;
+//         for(int i =0;i<nums.size()-1;i++){
+//             if(nums[i] == nums[i+1]) return true;
+//         }
+//         return flag;
+//     }
+// };
