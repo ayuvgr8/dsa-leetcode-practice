c8c458724da491dbab50eb11534edbaf908d5cc5
Ayush Verma <49532650+ayuvgr8@users.noreply.github.com>
2023-12-20 13:18:13 +0530

    Time: 86 ms (90.95%), Space: 57.7 MB (87.28%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -35,10 +35,29 @@
 // };
 
 
-// Set Approach
+
+
+
+// // Set Approach
+// class Solution {
+// public:
+//     bool containsDuplicate(vector<int>& nums) {
+//         return nums.size() > set<int>(nums.begin(),nums.end()).size();
+//     }
+// };
+
+
+
+
+// Shorted Approach
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        return nums.size() > set<int>(nums.begin(),nums.end()).size();
+        sort(nums.begin(),nums.end());
+        bool flag = false;
+        for(int i =0;i<nums.size()-1;i++){
+            if(nums[i] == nums[i+1]) return true;
+        }
+        return flag;
     }
 };
