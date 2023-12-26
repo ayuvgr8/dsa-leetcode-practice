0120242c7a97089291dd4b5ee42bdaa7494a84ff
Ayush Verma <49532650+ayuvgr8@users.noreply.github.com>
2023-12-26 14:44:25 +0530

    Time: 821 ms (83.98%), Space: 0B (100.00%) - LeetHub

--- 0585-investments-in-2016/0585-investments-in-2016.sql
+++ 0585-investments-in-2016/0585-investments-in-2016.sql
@@ -13,3 +13,18 @@ AND (lat, lon) IN (
     GROUP BY lat, lon
     HAVING COUNT(pid) = 1
 );
+
+
+# Explanation:
+
+# SELECT ROUND(SUM(tiv_2016), 2) AS tiv_2016: Selects the rounded sum of tiv_2016 values as tiv_2016 in the output.
+
+# WHERE tiv_2015 IN (...): Filters rows where tiv_2015 values are present in the subquery.
+
+# SELECT tiv_2015 FROM Insurance GROUP BY tiv_2015 HAVING COUNT(pid) > 1: Subquery finds tiv_2015 values that are shared by more than one policyholder.
+
+# AND (lat, lon) IN (...): Further filters rows based on unique location pairs.
+
+# SELECT lat, lon FROM Insurance GROUP BY lat, lon HAVING COUNT(pid) = 1: Subquery finds unique location pairs.
+
+# This query retrieves the sum of tiv_2016 for policyholders meeting both conditions: having the same tiv_2015 value as other policyholders and being in a unique location (unique latitude and longitude pair). The ROUND function ensures the result is rounded to two decimal places, as required in the output.
